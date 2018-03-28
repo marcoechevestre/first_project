91dcb44bf5326c8c06581c2e720e6cc4e94eab7f
Marco Minotto <marcoaminotto@hotmail.com>
2018-03-28 14:18:52 -0300

    Responses into soup controller
    - Added json and html response
    - Added a private method to fetch a single item
    - Added a link to categories page in the show.html.erb file

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,5 +1,40 @@
 class SoupsController < ApplicationController
+  before_action :fetch_soup, only: [:show, :update, :edit:, :destroy]
+  def index
+    @soups = Soup.all
+
+    respond_to do |format|
+      format.json {render json: @soups}
+    end
+  end
+
   def show
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @soup}
+    end
+  end
+
+  #do
+  def new
+  end
+
+  #do
+  def update
+  end
+
+  #do
+  def edit
+  end
+
+  #do
+  def destroy
+  end
+
+  private
+
+  def fetch_soup
     @soup = Soup.find(params[:id])
   end
 end
